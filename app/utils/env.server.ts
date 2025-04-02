4aa644c3cd8745db7181490ba762069ff626f4f4
Piyush Lunawat <16piyushl@gmail.com>
2025-04-02 21:41:08 +0530

    private

--- app/utils/env.server.ts
+++ app/utils/env.server.ts
@@ -7,14 +7,13 @@ const schema = z.object({
   AWS_ACCESS_KEY_ID: z.string(),
   AWS_SECRET_ACCESS_KEY: z.string(),
   AWS_REGION: z.string(),
-  FIREBASE_API_KEY: z.string(),
-  FIREBASE_AUTH_DOMAIN: z.string(),
-  FIREBASE_PROJECT_ID: z.string(),
-  FIREBASE_STORAGE_BUCKET: z.string(),
-  FIREBASE_MESSAGING_SENDER_ID: z.string(),
-  FIREBASE_APP_ID: z.string(),
-  FIREBASE_MEASUREMENT_ID: z.string(),
-  // You can add other variables here as needed, but only expose public variables to the client.
+  // FIREBASE_API_KEY: z.string(),
+  // FIREBASE_AUTH_DOMAIN: z.string(),
+  // FIREBASE_PROJECT_ID: z.string(),
+  // FIREBASE_STORAGE_BUCKET: z.string(),
+  // FIREBASE_MESSAGING_SENDER_ID: z.string(),
+  // FIREBASE_APP_ID: z.string(),
+  // FIREBASE_MEASUREMENT_ID: z.string(),
 });
 
 // Parse the environment variables and validate them
