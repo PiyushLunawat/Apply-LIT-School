ea9fda83c2e84b33da21799d32c53cf843edb743
Piyush Lunawat <16piyushl@gmail.com>
2025-04-03 22:34:12 +0530

    Live Server

--- app/config/fallbacks.ts
+++ app/config/fallbacks.ts
@@ -5,8 +5,8 @@
  * These are critical for early application initialization before React is ready
  */
 export const ENV_FALLBACKS = {
-  //   REMIX_PUBLIC_API_BASE_URL: "https://dev.apply.litschool.in",
-  REMIX_PUBLIC_API_BASE_URL: "http://localhost:4000",
+  REMIX_PUBLIC_API_BASE_URL: "https://dev.apply.litschool.in",
+  // REMIX_PUBLIC_API_BASE_URL: "http://localhost:4000",
   AWS_ACCESS_KEY_ID: "AKIA3A3WKZTB3X3X75FW",
   AWS_SECRET_ACCESS_KEY: "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",
   AWS_REGION: "eu-north-1",
